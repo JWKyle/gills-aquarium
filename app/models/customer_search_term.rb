cd3b5488f9554bff59852791b1138f14ae209878
Jim Kyle <jim@jwkyle.com>
2018-01-10 10:51:47 -0600

    add helpers to execute SQL in name search

--- app/models/customer_search_term.rb
+++ app/models/customer_search_term.rb
@@ -17,4 +17,23 @@ class CustomerSearchTerm
     end
   end
 end
- 
+
+private
+
+  def name_search(search_term)
+    @where_clause << case_insensitive_search(:first_name)
+    @where_args[:first_name] = starts_with(search_term)
+
+    @where_clause << " OR #{case_insensitive_search(:last_name)}"
+    @where_args[:last_name] = starts_with(search_term)
+
+    @order = "last_name asc"
+  end
+
+  def starts_with(search_term)
+    search_term + "%"
+  end
+
+  def case_insensitive_search(field_name)
+    "lower(#{field_name}) like :#{field_name}"
+  end
