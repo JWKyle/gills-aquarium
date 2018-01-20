ac66393a3be53f3775b077baf963a6487fc09a24
Jim Kyle <jim@jwkyle.com>
2018-01-19 22:51:49 -0600

    add ngFor

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -17,10 +17,12 @@ var CustomerSearchComponent = Component({
       <label for="keywords" class="sr-only">Keywords></label> \
       <input type="text" id="keywords" name="keywords" \
              placeholder="First Name, Last Name, or Email Address"\
-             class="form-control input-lg">\
+             class="form-control input-lg"\
+             bindon-ngModel="keywords">\
       <span class="input-group-btn"> \
         <input type="submit" value="Find Customers"\
-               class="btn btn-primary btn-lg">\
+               class="btn btn-primary btn-lg" \
+               on-click="search()"> \
       </span> \
     </div> \
   </form> \
@@ -46,6 +48,10 @@ var CustomerSearchComponent = Component({
   '
 }).Class({
   constructor: function() {
+    this.keywords = null;
+  },
+  search: function() {
+    alert("Searched for: " + this.keywords);
   }
 });
 
