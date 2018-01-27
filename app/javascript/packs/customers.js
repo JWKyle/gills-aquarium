8fd8227ee44b77cd08e002b879492400dfe18981
Jim Kyle <jim@jwkyle.com>
2018-01-26 19:13:55 -0600

    add typesearch, remove search button, remove hardcoded test info, tests green

--- app/javascript/packs/customers.js
+++ app/javascript/packs/customers.js
@@ -6,45 +6,6 @@ import { FormsModule            } from "@angular/forms";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { Http,HttpModule } from "@angular/http";
 
-var RESULTS = [
-  {
-    first_name: "Pat",
-    last_name: "Smith",
-    username: "psmith",
-    email: "pat.smith@somewhere.net",
-    created_at: "2016-02-05",
-  },
-  {
-    first_name: "Patrick",
-    last_name: "Jones",
-    username: "pjpj",
-    email: "jones.p@business.net",
-    created_at: "2014-03-05",
-  },
-  {
-    first_name: "Patricia",
-    last_name: "Benjamin",
-    username: "pattyb",
-    email: "benjie@aol.info",
-    created_at: "2016-01-02",
-  },
-  {
-    first_name: "Patty",
-    last_name: "Patrickson",
-    username: "ppat",
-    email: "pppp@freemail.computer",
-    created_at: "2016-02-05",
-  },
-  {
-    first_name: "Jane",
-    last_name: "Patrick",
-    username: "janesays",
-    email: "janep@company.net",
-    created_at: "2013-01-05",
-  },
-];
-
-
 var CustomerSearchComponent = Component({
   selector: "gills-customer-search",
   template: '\
@@ -53,18 +14,13 @@ var CustomerSearchComponent = Component({
 </header> \
 <section class="search-form"> \
   <form> \
-    <div class="input-group input-group-lg"> \
-      <label for="keywords" class="sr-only">Keywords></label> \
-      <input type="text" id="keywords" name="keywords" \
-             placeholder="First Name, Last Name, or Email Address"\
-             class="form-control input-lg"\
-             bindon-ngModel="keywords">\
-      <span class="input-group-btn"> \
-        <input type="submit" value="Find Customers"\
-               class="btn btn-primary btn-lg" \
-               on-click="search()"> \
-      </span> \
-    </div> \
+    <label for="keywords" class="sr-only">Keywords></label> \
+    <input type="text" id="keywords" name="keywords" \
+           placeholder="First Name, Last Name, or Email Address"\
+           class="form-control input-lg"\
+           bind-ngModel="keywords"\
+           on-ngModelChange="search($event)"\
+           class="form-control input-lg">\
   </form> \
 </section> \
 <section class="search-results"> \
@@ -96,8 +52,12 @@ var CustomerSearchComponent = Component({
     this.keywords = "";
     }
   ],
-  search: function() {
+  search: function($event) {
     var self = this;
+    self.keywords = $event;
+    if (self.keywords.length < 3) {
+      return;
+    }
     self.http.get(
       "/customers.json?keywords=" + self.keywords
     ).subscribe(
