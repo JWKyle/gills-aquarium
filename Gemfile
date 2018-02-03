8a6fc6a63e11cf37fb06db7d9921e5ec8a43363d
Jim Kyle <jim@jwkyle.com>
2018-02-03 15:26:06 -0600

    edit tests for test-debug

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.4'
 # Use webpacker to use Webpack
-gem 'webpacker'
+gem 'webpacker', '~> 3.0'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
--- spec/features/customer_search_spec.rb
+++ spec/features/customer_search_spec.rb
@@ -23,8 +23,36 @@ feature "Customer Search" do
     create_customer first_name: "Charlie", last_name: "Cuddlefish"
     create_customer first_name: "Danielle", last_name: "Dory"
     create_customer first_name: "Elvis", last_name: "Eel"
-    create_customer first_name: "Harriett", last_name: "Halibut", email: "halibut4life@online.com"
+    create_customer first_name: "Harrison", last_name: "Haddock"
+    create_customer first_name: "Helen", last_name: "Hartail"
+    #the following should be our search result
+    create_customer first_name: "Har", last_name: "Halibut", email: "halibut4life@online.com"
 
+  end
+
+  scenario "Search By Name"do
+    visit "/customers"
+
+    #login test
+
+    fill_in "Email", with: email
+    fill_in "Password", with: password
+    click_button "Log in"
+puts page.html #debug
+    within "section.search-form" do
+      fill_in "keywords", with: "har"
+    end
+
+    within "section.search-results" do
+      expect(page).to have_content("Results")
+      expect(page.all("ol li.list-group-item").count).to eq(8)
+      list_group_items = page.all("ol li.list-group-item")
+
+      expect(list_group_items[0]).to have_content("Harrison")
+      expect(list_group_items[0]).to have_content("Haddock")
+      expect(list_group_items[2]).to have_content("Helen")
+      expect(list_group_items[2]).to have_content("Hartail")
+    end
   end
 
 
